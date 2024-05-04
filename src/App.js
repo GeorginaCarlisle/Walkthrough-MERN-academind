9b800bb82d556806726dd07d8826353d52c679de
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-05-04 21:17:30 +0100

    FEAT: Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Users/>} />
+        <Route path="/places/new" element={<NewPlace/>} />
+        {/* the /* path will pick up any urls for which there is no matching path */}
+        <Route path="/*" element={<Users/>} />
+        {/* A different option: <Route path="/*" element={<Navigate to="/" replace />} /> */}
+        {/* 404 page is probably a good idea here */}
+
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
