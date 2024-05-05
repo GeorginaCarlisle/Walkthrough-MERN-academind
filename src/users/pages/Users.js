fe6a39535a214c732410802d3f7ca66d73e28797
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-05-05 11:30:55 +0100

    FEAT: Add base components for the user page

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+
+import UsersList from '../components/UsersList';
+
 
 const Users = () => {
+  const USERS = [{id: 'u1', name: 'Georgina', image: 'https://www.pexels.com/photo/woman-in-yellow-long-sleeve-standing-under-the-sunlight-3779756/', places: 3}]
   return (
-    <div>Users</div>
-  )
+    <UsersList items={USERS} />
+  );
 }
 
 export default Users
