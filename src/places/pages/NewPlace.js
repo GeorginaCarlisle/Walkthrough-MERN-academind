3f82396491534cb01c3d2c23fd67217b3d88e84c
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-05-11 19:11:36 +0100

    FEAT: Add input, validation and submission logic plus extra inputs

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,20 +1,99 @@
-import React from 'react';
+import React, { useCallback, useReducer } from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
-import { VALIDATOR_REQUIRE } from '../../shared/components/util/validators';
+import Button from '../../shared/components/FormElements/Button';
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/components/util/validators';
 import './NewPlace.css';
 
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      let formIsValid = true;
+      // Looping through all inputs held in state - see useReducer statement further down
+      for (const inputId in state.inputs) {
+        // Check if the input being looped through is the same as the input that has been changed and has triggered this function
+        if (inputId === action.inputId) {
+          //update formisvalid status based on whether the new input is valid AND nothing has changed form is valid to false
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          //update formisvalid status based on whether stored input is valid AND nothing has changed form is valid to false
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid }
+        },
+        isValid: formIsValid
+      };
+    default:
+      return state;
+  }
+};
+
 const NewPlace = () => {
+
+  // This handles the validity of the full form
+  const [formState, dispatch] = useReducer(formReducer, {
+    // in here we set the initial state
+    inputs: {
+      title: {
+        value: '',
+        isValid: false
+      },
+      description: {
+        value: '',
+        isValid: false
+      }
+    },
+    isValid: false
+  });
+
+  // Need to spend sometime getting to properly understand useCallback and why it is needed to prevent infinite loops
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: 'INPUT_CHANGE',
+      value: value,
+      isValid: isValid,
+      inputId: id
+    });
+  }, []);
+
+  const placeSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
   return (
-    <form className='place-form'>
+    <form className='place-form' onSubmit={placeSubmitHandler}>
       <Input
+        id="title"
         element='input'
         type="text"
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title"
+        onInput={inputHandler}
       />
-
+      <Input
+        id="description"
+        element='textarea'
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        element='input'
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address."
+        onInput={inputHandler}
+      />
+      <Button type="submit" disabled={!formState.isValid}>ADD PLACE</Button>
     </form>
   )
 }
