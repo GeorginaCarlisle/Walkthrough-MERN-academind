39422666ca3856d2af59764c37d08bafe507da03
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-06-17 12:31:07 +0100

    FEAT: Signup route connected to backend

--- src/users/pages/Auth.js
+++ src/users/pages/Auth.js
@@ -42,9 +42,32 @@ const Auth = () => {
     setIsLogin(prevMode => !prevMode);
   };
 
-  const loginSubmitHandler = event => {
+  const loginSubmitHandler = async event => {
     event.preventDefault();
-    console.log(formState.inputs);
+
+    if (isLogin) {
+
+    } else {
+      try {
+        const response = await fetch('http://localhost:5000/api/users/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value
+          })
+        });
+        const responseData = await response.json();
+        console.log(responseData);
+      } catch (err){
+        console.log(err);
+      }
+      
+    }
+
     auth.login();
   };
 
