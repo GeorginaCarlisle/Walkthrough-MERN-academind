72c89359fe9f7f6a8775087278313f334ba8e1c9
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-06-18 11:36:15 +0100

    FEAT: Functionality to create a new place in the database

--- src/users/pages/Auth.js
+++ src/users/pages/Auth.js
@@ -52,7 +52,7 @@ const Auth = () => {
 
     if (isLogin) {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           'http://localhost:5000/api/users/login',
           'POST',
           JSON.stringify({
@@ -63,11 +63,11 @@ const Auth = () => {
             'Content-Type': 'application/json'
           }
         );
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err){}
     } else {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           'http://localhost:5000/api/users/signup',
           'POST',
           JSON.stringify({
@@ -79,7 +79,7 @@ const Auth = () => {
             'Content-Type': 'application/json'
           }
         );
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {}
     }
   };
@@ -130,9 +130,7 @@ const Auth = () => {
         </Button>
       </Card>
     </React.Fragment>
-    
-    
   )
 }
 
-export default Auth
+export default Auth;
