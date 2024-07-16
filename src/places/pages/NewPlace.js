2054b990226917038204a7d01b66bcd6b79d2cd8
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-07-16 20:59:35 +0100

    FEAT: Add token authentication

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -49,7 +49,8 @@ const NewPlace = () => {
       await sendRequest(
         'http://localhost:5000/api/places',
         'POST',
-        formData
+        formData,
+        { Authorization: 'Bearer ' + auth.token }
       );
       history('/');
     } catch (err) {}
